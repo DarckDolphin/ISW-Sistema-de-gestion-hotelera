c3505dda95888426960e757afd35a861c5658225
Oniel Milian <onimilian28@gmail.com>
2025-05-03 20:34:53 -0400

    vamos a darle ya francisco

--- hotel_frontend/src/App.jsx
+++ hotel_frontend/src/App.jsx
@@ -1,17 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/login';
-import Dashboard from './pages/dashboard';
-import Reservas from './pages/reservas';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import Reservas from "./pages/reservas";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/reservas" element={<Reservas />} />
-      </Routes>
-    </Router>
+    <>
+      <h1>Ya ya estoy aqui tanq</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/reservas" element={<Reservas />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
