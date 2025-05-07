50be667c6f4650c1ff20cddba4170035d12d026b
oniel_milian03 <134478023+onielmilian@users.noreply.github.com>
2025-05-07 17:15:41 -0400

    Update App.jsx

--- hotel_frontend/src/App.jsx
+++ hotel_frontend/src/App.jsx
@@ -6,8 +6,6 @@ import Reservas from "./pages/reservas";
 function App() {
   return (
     <>
-      <h1>Ya ya estoy aqui tanq</h1>
-      <p>Francis tenme paciencia q vengo a ver git ayer lol</p>
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
